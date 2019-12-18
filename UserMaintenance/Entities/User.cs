83c450daaa8d59304162ac947ebdf916807bb7a9
Szitásné Paár Zsófia <zsofi.paar@gmail.com>
2019-12-18 17:55:41 +0100

    hozzaadas funkcio

--- UserMaintenance/Entities/User.cs
+++ UserMaintenance/Entities/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace UserMaintenance.Entities
 {
-    class User
+    public class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
         public string FirstName { get; set; }
--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -7,17 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+        
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName; // label1
             label2.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
+
+            // listbox1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
